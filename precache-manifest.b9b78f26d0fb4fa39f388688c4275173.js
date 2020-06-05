cfd65aa0f738b1005438ce1c0c9196a2d5eda96b
Admin <janviermbilizi@gmail.com>
2020-06-04 19:29:23 -0600

    Updates

---
+++ precache-manifest.b9b78f26d0fb4fa39f388688c4275173.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6f6e0783a8b519f7cfa",
+    "url": "/entertain-frontEnd/static/css/main.15f47786.chunk.css"
+  },
+  {
+    "revision": "a6f6e0783a8b519f7cfa",
+    "url": "/entertain-frontEnd/static/js/main.be6377df.chunk.js"
+  },
+  {
+    "revision": "806fa4ccb8719d5679c3",
+    "url": "/entertain-frontEnd/static/js/runtime~main.13ab5590.js"
+  },
+  {
+    "revision": "0a3c247171d3ec556e64",
+    "url": "/entertain-frontEnd/static/js/2.e604c15c.chunk.js"
+  },
+  {
+    "revision": "2608d598cd745dcccb6a9dd12c53d32c",
+    "url": "/entertain-frontEnd/static/media/logo.2608d598.png"
+  },
+  {
+    "revision": "44b7e5e46851cbe399a5be7f5f91da66",
+    "url": "/entertain-frontEnd/static/media/countdown.44b7e5e4.gif"
+  },
+  {
+    "revision": "9c3f413fadff18e9b0249742bdac984a",
+    "url": "/entertain-frontEnd/static/media/scoob.9c3f413f.jpg"
+  },
+  {
+    "revision": "c8e356638709607823a8f98b9e8474a1",
+    "url": "/entertain-frontEnd/static/media/red-carpet.c8e35663.jpg"
+  },
+  {
+    "revision": "9acfe78b8e1cfbf4c1b1d1f31745b96b",
+    "url": "/entertain-frontEnd/static/media/blank-profile-picture.9acfe78b.png"
+  },
+  {
+    "revision": "a0d859433ea08d51e7dfa65917eb1a76",
+    "url": "/entertain-frontEnd/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/entertain-frontEnd/precache-manifest.b9b78f26d0fb4fa39f388688c4275173.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/entertain-frontEnd/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
