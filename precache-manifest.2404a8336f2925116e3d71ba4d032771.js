e41d69c1dc86fba2e909102dd7c1308d78b268e5
Admin <janviermbilizi@gmail.com>
2020-06-04 21:32:23 -0600

    Updates

---
+++ precache-manifest.2404a8336f2925116e3d71ba4d032771.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac9dd51b944ce6259b5acb162ff30d0",
+    "url": "/entertain-frontEnd/index.html"
+  },
+  {
+    "revision": "326c057a9a352851890f",
+    "url": "/entertain-frontEnd/static/css/main.ba34f4b5.chunk.css"
+  },
+  {
+    "revision": "d6ee87afecfaa6a9631c",
+    "url": "/entertain-frontEnd/static/js/2.5b42f816.chunk.js"
+  },
+  {
+    "revision": "9cd217ed51c00cfcd07b604ad673937f",
+    "url": "/entertain-frontEnd/static/js/2.5b42f816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "326c057a9a352851890f",
+    "url": "/entertain-frontEnd/static/js/main.9d7362c1.chunk.js"
+  },
+  {
+    "revision": "4d554922f6997e5ac5f6",
+    "url": "/entertain-frontEnd/static/js/runtime-main.6615cb86.js"
+  },
+  {
+    "revision": "9acfe78b8e1cfbf4c1b1d1f31745b96b",
+    "url": "/entertain-frontEnd/static/media/blank-profile-picture.9acfe78b.png"
+  },
+  {
+    "revision": "44b7e5e46851cbe399a5be7f5f91da66",
+    "url": "/entertain-frontEnd/static/media/countdown.44b7e5e4.gif"
+  },
+  {
+    "revision": "2608d598cd745dcccb6a9dd12c53d32c",
+    "url": "/entertain-frontEnd/static/media/logo.2608d598.png"
+  },
+  {
+    "revision": "c8e356638709607823a8f98b9e8474a1",
+    "url": "/entertain-frontEnd/static/media/red-carpet.c8e35663.jpg"
+  },
+  {
+    "revision": "9c3f413fadff18e9b0249742bdac984a",
+    "url": "/entertain-frontEnd/static/media/scoob.9c3f413f.jpg"
+  }
+]);
